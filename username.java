5a6f42b230258eaa2d31efbdb5e8c16b8ed0626b
SaiTarunbandi <158998863+SaiTarunbandi@users.noreply.github.com>
2024-02-09 19:40:25 +0530

    Update username.java

--- username.java
+++ username.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
-class Username
-{
-  public static void main(String[] args)
-  {
-    String user;
-    System.out.print("Enter the user name : ");
-    Scanner input=new Scanner(System.in);
-    user=input.nextLine();
-    input.close();
-    System.out.print("Hello, "+user);
-  }
+
+public class username {
+
+	public static void main(String[] args) {
+
+		System.out.println("Please enter your name:");
+
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+
+		System.out.println("Hello " + name);
+
+	}
+
 }
